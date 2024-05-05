f17d85b35ab64db46cf60e91a1eef0bf62537429
eunhachingu <espajunarjr@gmail.com>
2024-05-06 00:56:26 +0800

    Fix location and secret

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,32 +1,43 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:zavod_demo/widgets/drawer/custom_drawer.dart';
 
 @RoutePage()
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   static LatLng googlePlex = const LatLng(37.4223, -122.0848);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const LatLng _kGooglePlex = LatLng(37.42796133580664, -122.085749655962);
+  static const LatLng _mountainView = LatLng(38.42796133580664, -122.085749655962);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: googlePlex,
-          zoom: 13,
+        mapType: MapType.hybrid,
+        initialCameraPosition: const CameraPosition(
+          target: _kGooglePlex,
+          zoom: 13.0,
         ),
         markers: {
-          Marker(
-            markerId: const MarkerId("sourceLocation"),
+          const Marker(
+            markerId: MarkerId("sourceLocation"),
             icon: BitmapDescriptor.defaultMarker,
-            position: googlePlex,
+            position: _kGooglePlex,
+          ),
+          const Marker(
+            markerId: MarkerId("destinationLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _mountainView,
           ),
         },
       ),
-      drawer: const CustomDrawer(),
     );
   }
 }
